7d6c9de2b1b260118adbbadbc57a09485617820f
Kyoto67 <whynot.2@icloud.com>
2024-11-04 19:26:50 +0300

    Fix: tests

--- apps/interviewing-service/src/test/java/ru/itmo/cs/TestcontainersConfiguration.java
+++ apps/interviewing-service/src/test/java/ru/itmo/cs/TestcontainersConfiguration.java
@@ -1,6 +1,13 @@
 package ru.itmo.cs;
 
+import java.util.List;
+
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
+import ru.ifmo.cs.domain_event.infrastructure.repository.KnownDomainEventTypeResolver;
+import ru.itmo.cs.app.interviewing.configuration.InterviewingServiceConfiguration;
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
