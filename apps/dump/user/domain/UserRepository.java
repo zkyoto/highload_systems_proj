33023f5381ad4c5a16fc737f7b050f233a0b3e92
DeadInsideDora <dallvoro678@gmail.com>
2024-11-09 15:41:52 +0300

    remove backup

--- apps/dump/user/domain/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.ifmo.cs.api_gateway.user.domain;
-
-import ru.ifmo.cs.api_gateway.user.domain.value.Username;
-
-public interface UserRepository {
-    User findByUsername(Username username);
-    void save(User user);
-}
--- apps/dump/user/presentation/dto/RegisterUserRequestBodyDto.java
+++
@@ -1,9 +0,0 @@
-package ru.ifmo.cs.api_gateway.user.presentation.dto;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record RegisterUserRequestBodyDto(
-        @JsonProperty("username") String username,
-        @JsonProperty("password") String password,
-        @JsonProperty("role") String role
-) {}
