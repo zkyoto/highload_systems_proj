dabb75e9828f168f1110f29d07556e34a2359ab0
VxDxK <vadim.v.ponomarev@gmail.com>
2025-01-09 09:23:05 +0300

    jacoco fix

--- apps/interviewing-service/interviews/src/main/java/ru/ifmo/cs/interviews/configuration/EventsConfiguration.java
+++ apps/interviewing-service/interviews/src/main/java/ru/ifmo/cs/interviews/configuration/EventsConfiguration.java
@@ -5,13 +5,9 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import ru.ifmo.cs.contracts.interviewing_service.interview_results.integration_event.InterviewResultCreatedIntegrationEvent;
 import ru.ifmo.cs.domain_event.configuration.DomainEventsConfiguration;
 import ru.ifmo.cs.domain_event.infrastructure.repository.KnownDomainEventTypeResolver;
-import ru.ifmo.cs.integration_event.configuration.IntegrationEventDeliveryConfiguration;
 import ru.ifmo.cs.integration_event.configuration.IntegrationEventPublishingConfiguration;
-import ru.ifmo.cs.integration_event.event_delivery.KnownIntegrationEvent;
-import ru.ifmo.cs.integration_event.event_delivery.KnownIntegrationEventTypeResolver;
 import ru.ifmo.cs.interviews.application.service.event.InterviewEventsKnownEventProvider;
 
 @Configuration
