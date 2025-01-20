bdb32a0f5f4235b0a2b5dc71aca062858a32808f
Kyoto67 <whynot.2@icloud.com>
2025-01-20 20:57:03 +0300

    another try

--- common/libs/synchronization/src/main/java/ru/ifmo/cs/synchronization/configuration/HazelCastConfiguration.java
+++ common/libs/synchronization/src/main/java/ru/ifmo/cs/synchronization/configuration/HazelCastConfiguration.java
@@ -1,7 +1,7 @@
 package ru.ifmo.cs.synchronization.configuration;
 
-import com.hazelcast.config.Config;
-import com.hazelcast.core.Hazelcast;
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.map.IMap;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -13,13 +13,21 @@ import java.time.LocalDateTime;
 @Configuration
 public class HazelCastConfiguration {
 
+    private static final String HAZELCAST_SERVICE_NAME = "hz-hazelcast";
+
     @Bean
     public HazelcastInstance hazelcastInstance() {
-        Config config = new Config();
-        config.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
-        config.getNetworkConfig().getJoin().getKubernetesConfig().setEnabled(true);
-        return Hazelcast.newHazelcastInstance(config);
+        final ClientConfig config = new ClientConfig();
+        config.setClusterName("dev");
+
+        config.getNetworkConfig().getKubernetesConfig()
+                .setEnabled(true)
+                .setProperty("namespace", "default")
+                .setProperty("service-name", HAZELCAST_SERVICE_NAME);
+
+        return HazelcastClient.newHazelcastClient(config);
     }
+
 
     @Bean
     public IMap<String, LocalDateTime> timeMap(@Qualifier("hazelcastInstance") HazelcastInstance hazelcastInstance) {
